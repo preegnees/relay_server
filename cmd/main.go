d961bdeadeed61e66c528a9733e9d258c36dfb41
preegnees <mrraziapov13@gmail.com>
2022-06-30 15:16:33 +0300

    Написал точку старта приложения. Парсер флагов, для дебага, инициализация логгера. Вместо функции main, написал run(), чтобы возвращать ощибку, там происходит чтение конфига.

---
+++ cmd/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+
+	"relay_server/pkg/config"
+
+	"relay_server/pkg/logger"
+)
+
+// точка входа
+func run(log logger.ILogger) error {
+	cnf, err := config.Get()
+	if err != nil {
+		return fmt.Errorf("$Ошибка при чтении конфига. Err: %v", err)
+	}
+	if cnf == nil {
+		return fmt.Errorf("$Невалидный конфиг. Cnf: %v", cnf)
+	}
+	log.Debug(fmt.Sprintf("Cnf: %v", cnf))
+	return nil
+}
+
+// структура для описания флагов
+type Flags struct {
+	Debug bool
+}
+
+// парсер флагов
+func parseFlags() *Flags {
+	var dbg = flag.Bool("d", false, "is debug???")
+	flag.Parse()
+	flgs := &Flags{
+		Debug: *dbg,
+	}
+	return flgs
+}
+
+// инициализация логгер, атребут дебаг
+func initLogger(flgs *Flags) logger.ILogger {
+	var log logger.ILogger = &logger.MyLogger{
+		Dbg: flgs.Debug,
+	}
+	return log
+}
+
+func main() {
+	flgs := parseFlags()
+	log := initLogger(flgs)
+	log.Debug("Старт")
+	if err := run(log); err != nil {
+		log.Error(fmt.Sprintf("$Критическая ошибка. Err: %v", err))
+	}
+}

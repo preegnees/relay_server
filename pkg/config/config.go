3b8837c44c66594d2a7aca06eb4f590b369ebddd
preegnees <mrraziapov13@gmail.com>
2022-06-30 15:19:30 +0300

    Чтение env файл с помощью библиотеки github.com/joho/godotenv

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	fmt "fmt"
+	os "os"
+	strconv "strconv"
+
+	godotenv "github.com/joho/godotenv" 
+)
+ 
+type config struct {
+	Port int8
+}
+
+func Get() (*config, error) {
+	err := godotenv.Load("config.env")
+	if err != nil {
+		return nil, fmt.Errorf("$Невозможно прочитать файл конфигурации. err: %v", err)
+	}
+	port, err := strconv.ParseInt(os.Getenv("port"), 10, 8)
+	if err != nil {
+		return nil, fmt.Errorf("$Невалидный порт. err: %v", err)
+	}
+	cnf := config{
+		Port: int8(port),
+	}
+	return &cnf, nil
+}

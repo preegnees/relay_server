3635aeed19989cdebba899907c8ff97f40578479
preegnees <mrraziapov13@gmail.com>
2022-07-01 13:55:21 +0300

    Написал тест для подключения к mongodb

--- pkg/repo/storage/mongo/connector_test.go
+++ pkg/repo/storage/mongo/connector_test.go
@@ -2,17 +2,21 @@ package mongo
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type fakeLog struct{}
 
-func (t *fakeLog) Info(s string)  {}
-func (t *fakeLog) Error(s string) {}
-func (t *fakeLog) Debug(s string) {}
+func (t *fakeLog) Info(s interface{})  {}
+func (t *fakeLog) Error(s interface{}) {}
+func (t *fakeLog) Debug(s interface{}) {
+	fmt.Println(s)
+}
 
 func Test_NewClient_Connection(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -20,14 +24,21 @@ func Test_NewClient_Connection(t *testing.T) {
 		Log:      &fakeLog{},
 		Ctx:      ctx,
 		Host:     "localhost",
-		Port:     "8888",
+		Port:     "27017",
 		Username: "",
 		Password: "",
-		DB:       "test_db",
+		DB:       "radmir",
 	}
 
-	database, err := NewClient(cnf)
+	client, err := NewClient(cnf)
 	assert.True(t, err == nil)
-	assert.True(t, database != nil)
-	cancel()
+	assert.True(t, client != nil)
+
+	database, err := client.ListDatabaseNames(cnf.Ctx, bson.M{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(database)
+	defer cancel()
+	defer client.Disconnect(cnf.Ctx)
 }

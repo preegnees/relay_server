eee9f7d7658f5f5a3ffb4634e19ac3b0ab38381e
preegnees <mrraziapov13@gmail.com>
2022-07-01 10:08:07 +0300

    Доавил структуры для информации о хосте для jsson и bson, так же написал dto для json

--- pkg/repo/storage/repository.go
+++ pkg/repo/storage/repository.go
@@ -1,20 +1,37 @@
 package repo
 
+import (
+	"context"
+
+	"relay_server/pkg/logger"
+)
+
 // Для хранения информации о хосте
 type Host struct {
-	ID       string
-	IP       string
-	GoOS     string
-	Kernel   string
-	Core     string
-	Platform string
-	OS       string
-	Hostname string
-	CPUs     string
+	ID       string `json:"id" bson:"id"`
+	GoOS     string `json:"go_os" bson:"go_os"`
+	Kernel   string `json:"kernel" bson:"kernel"`
+	Core     string `json:"core" bson:"core"`
+	Platform string `json:"platform" bson:"platform"`
+	OS       string `json:"os" bson:"os"`
+	Hostname string `json:"hostanme" bson:"hostanme"`
+	CPUs     string `json:"cpus" bson:"cpus"`
+	IP       string `json:"ip" bson:"ip"`
+}
+
+type CreateHostDTO struct {
+	ID       string `json:"id"`
+	GoOS     string `json:"go_os"`
+	Kernel   string `json:"kernel"`
+	Core     string `json:"core"`
+	Platform string `json:"platform"`
+	OS       string `json:"os"`
+	Hostname string `json:"hostanme"`
+	CPUs     string `json:"cpus"`
 }
 
 // Для сохранение и взымании информации о хосте
 type IInfoHost interface {
-	GetInfoHost(string) (Host, error)
-	SaveInfoHost(Host) error
+	GetInfoHost(context.Context, logger.ILogger, string) (*Host, error)
+	SaveInfoHost(context.Context, *Host) error
 }

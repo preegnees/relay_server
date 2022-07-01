42394ddfb78995610f77f2ee8d1abc65f1a47530
preegnees <mrraziapov13@gmail.com>
2022-07-01 10:45:35 +0300

    В стурктура MongoDB доавил поле с коллекциями, чтобы в дальнейшем с ним работать

--- pkg/repo/storage/mongo/mongo.go
+++ pkg/repo/storage/mongo/mongo.go
@@ -5,13 +5,18 @@ import (
 
 	"relay_server/pkg/logger"
 	repo "relay_server/pkg/repo/storage"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type MongoDB struct {}
+type MongoDB struct {
+	Collection *mongo.Collection
+}
 
 var mongoTest repo.IInfoHost = (*MongoDB)(nil)
 
 func (m *MongoDB) GetInfoHost(ctx context.Context, log logger.ILogger, id string) (*repo.Host, error) { 
+	
 	return &repo.Host{}, nil
 }
 

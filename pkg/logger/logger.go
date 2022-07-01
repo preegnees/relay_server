99fc45aeea25e7ea5364e6493a16ed71a3f9b05d
preegnees <mrraziapov13@gmail.com>
2022-07-01 13:53:31 +0300

    Изменил типа аргумента string -> interface{}

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -6,25 +6,27 @@ import (
 )
 
 type ILogger interface {
-	Info(string)
-	Debug(string)
-	Error(string)	
+	Info(interface{})
+	Debug(interface{})
+	Error(interface{})	
 }
 
 type MyLogger struct {
 	Dbg bool
 }
 
-func (mlg *MyLogger) Info(s string) {
+func (mlg *MyLogger) Info(s interface{}) {
 	log.Println(fmt.Sprintf("INFO. %s", s))
 }
 
-func (mlg *MyLogger) Error(s string) {
+func (mlg *MyLogger) Error(s interface{}) {
 	log.Fatal(fmt.Sprintf("ERROR. %s", s))
 }
 
-func (mlg *MyLogger) Debug(s string) {
+func (mlg *MyLogger) Debug(s interface{}) {
 	if mlg.Dbg {
 		log.Println(fmt.Sprintf("DEBUG. %s", s))
 	}
 }
+
+var test ILogger = (*MyLogger)(nil)

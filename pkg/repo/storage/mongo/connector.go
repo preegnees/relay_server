f3df2602c0de67b2662c9b7fb8707f1bc25ac785
preegnees <mrraziapov13@gmail.com>
2022-07-03 02:45:32 +0300

    Добавил еще один контекст, который, по идее должен отключать клиента от БД, но почему-то этого не делает

--- pkg/repo/storage/mongo/connector.go
+++ pkg/repo/storage/mongo/connector.go
@@ -3,22 +3,17 @@ package mongo
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 	"relay_server/pkg/logger"
+	"syscall"
+
+	cnf "relay_server/pkg/config"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
-
-type CnfMongo struct {
-	Log      logger.ILogger
-	Ctx      context.Context
-	Host     string
-	Port     string
-	Username string
-	Password string
-	DB       string
-}
 
 const (
 	ErrorNewClientMongo = "$Ошибка создания клиента базы данных mongodb"
@@ -28,45 +23,73 @@ const (
 	ErrorGetDatabase = "$Ошибка получения базы данных"
 )
 
-func NewClient(cnf *CnfMongo) (*mongo.Client, error) {
+func New(ctx context.Context, ctxDisconn context.Context, log logger.ILogger) (*mongo.Database, error) {
 	var URI string
 	var credential options.Credential
-	var client *mongo.Client 
+	var client *mongo.Client
 	var err error
 
-	if cnf.Username == "" && cnf.Password == "" {
-		URI = fmt.Sprintf("mongodb://%s:%s", cnf.Host, cnf.Port)
+	if os.Getenv(cnf.ConfMongoUsername) == "" || os.Getenv(cnf.ConfMongoPassword) == "" {
+		URI = fmt.Sprintf("mongodb://%s:%s",
+			os.Getenv(cnf.ConfMongoHost),
+			os.Getenv(cnf.ConfMongoPort))
 		client, err = mongo.NewClient(options.Client().ApplyURI(URI))
 	} else {
-		URI = fmt.Sprintf("mongodb://%s:%s@%s:%s", cnf.Username, cnf.Password, cnf.Host, cnf.Port)
+		URI = fmt.Sprintf("mongodb://%s:%s@%s:%s",
+			os.Getenv(cnf.ConfMongoUsername),
+			os.Getenv(cnf.ConfMongoPassword),
+			os.Getenv(cnf.ConfMongoHost),
+			os.Getenv(cnf.ConfMongoPort))
 		credential = options.Credential{
-			Username: cnf.Username,
-			Password: cnf.Password,
+			Username: os.Getenv(cnf.ConfMongoUsername),
+			Password: os.Getenv(cnf.ConfMongoPassword),
 		}
 		client, err = mongo.NewClient(options.Client().ApplyURI(URI).SetAuth(credential))
 	}
-	
-	cnf.Log.Debug(fmt.Sprintf("URL mongo: %s", URI))
+
+	log.Debug(fmt.Sprintf("URL mongo: %s", URI))
 
 	if err != nil {
 		return nil, fmt.Errorf("%s. Err: %v", ErrorNewClientMongo, err)
 	}
 
-	cnf.Log.Debug(fmt.Sprintf("Успешное создание клиента"))
+	log.Debug(fmt.Sprintf("Успешное создание клиента"))
 
-	err = client.Connect(cnf.Ctx)
+	err = client.Connect(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("%s. Err: %v", ErrorConnectMongo, err)
 	}
 
-	cnf.Log.Debug(fmt.Sprintf("Успешное подключение"))
+	log.Debug(fmt.Sprintf("Успешное подключение к базе данных"))
+	log.Info("Успешное подключение к базе данных")
 
-	err = client.Ping(cnf.Ctx, readpref.Primary())
+	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
 		return nil, fmt.Errorf("%s. Err: %v", ErrorPingMongo, err)
 	}
 
-	cnf.Log.Debug(fmt.Sprintf("Успешный пинг"))
-	
-	return client, nil
+	log.Debug(fmt.Sprintf("Успешный пинг"))
+
+	database := client.Database(os.Getenv(cnf.ConfMongoDBName))
+
+	go func(ctxDisconn context.Context, cli *mongo.Client) {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+		ctx := context.Background()
+
+		disconn := func() {
+			cli.Disconnect(ctx)
+			log.Debug("Отлючено от базы данных")
+		}
+		
+		select {
+		case <-ctxDisconn.Done():
+			disconn()
+		case <-quit:
+			disconn()
+		}
+	}(ctxDisconn, client)
+
+	return database, nil
 }
